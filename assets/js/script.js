2320a246aa63c4c937b4f58f1f9eb76c6a2a878b
unknown <kimberly.zai@berkeley.edu>
2017-04-25 22:11:48 -0700

    Added workshops/team page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,38 +2,38 @@ $(document).ready(function() {
 
   // QUESTION 6
 
-  //Implement the showing and hiding of the sidebar when the user clicks on #sidebar-button here:
-  $("#sidebar-button").click(function() {
-    if ($(".sidebar-container").hasClass("sidebar-active")) {
-      $("body").removeClass("no-scroll");
-      $("#sidebar-button").removeClass("button-active");
-      $(".sidebar-container").removeClass("sidebar-active");
-      $(".page-wrapper").removeClass("wrapper-active");
-    } else {
-      setTimeout(function() {
-        $('body').addClass('no-scroll');
-      }, 300);
-
-      $("#sidebar-button").addClass("button-active");
-      $(".sidebar-container").addClass("sidebar-active");
-      $(".page-wrapper").addClass("wrapper-active");
-    }
-  });
-  // QUESTION 7
-
-  //Implement the hiding of the sidebar when the user clicks on the page wrapper here:
-  $(".page-wrapper").click(function() {
-    $("body").removeClass("no-scroll");
-    $("#sidebar-button").removeClass("button-active");
-    $(".sidebar-container").removeClass("sidebar-active");
-    $(".page-wrapper").removeClass("wrapper-active");
-  });
+  // //Implement the showing and hiding of the sidebar when the user clicks on #sidebar-button here:
+  // $("#sidebar-button").click(function() {
+  //   if ($(".sidebar-container").hasClass("sidebar-active")) {
+  //     $("body").removeClass("no-scroll");
+  //     $("#sidebar-button").removeClass("button-active");
+  //     $(".sidebar-container").removeClass("sidebar-active");
+  //     $(".page-wrapper").removeClass("wrapper-active");
+  //   } else {
+  //     setTimeout(function() {
+  //       $('body').addClass('no-scroll');
+  //     }, 300);
+  //
+  //     $("#sidebar-button").addClass("button-active");
+  //     $(".sidebar-container").addClass("sidebar-active");
+  //     $(".page-wrapper").addClass("wrapper-active");
+  //   }
+  // });
+  // // QUESTION 7
+  //
+  // //Implement the hiding of the sidebar when the user clicks on the page wrapper here:
+  // $(".page-wrapper").click(function() {
+  //   $("body").removeClass("no-scroll");
+  //   $("#sidebar-button").removeClass("button-active");
+  //   $(".sidebar-container").removeClass("sidebar-active");
+  //   $(".page-wrapper").removeClass("wrapper-active");
+  // });
   // QUESTION 8
 
   //Implement the "slide to left" when the user clicks on #carousel-next here
   $("#carousel-next").click(function() {
     var margin = parseInt($('#carousel').css('margin-left').replace("px", ""));
-    if (margin == -3840) {
+    if (margin == -2880) {
       return false;
     } else {
       $("#carousel").css("margin-left", margin - 960);
